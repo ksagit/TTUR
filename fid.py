e4a4f2e98792f41345dcae9eb9b1edbab92e2fab
KyleSargent <kylesargent8@gmail.com>
2019-02-17 19:23:39 -0500

    Fixed a bug in fid.py

--- fid.py
+++ fid.py
@@ -59,7 +59,7 @@ def _get_inception_layer(sess):
                   new_shape.append(None)
                 else:
                   new_shape.append(s)
-              o.__dict__['_shape_val'] = tf.TensorShape(new_shape)
+              o._shape = tf.TensorShape(new_shape)
     return pool3
 #-------------------------------------------------------------------------------
 
